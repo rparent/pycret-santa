f715b231b7499464c4deecf4791082e39f1f606d
Renaud Parent <renaud.parent@gmail.com>
2013-12-20 11:53:18 +0100

    Update version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 SETTINGS = dict(
   name = "pycret-santa",
-  version = "0.2",
+  version = "0.3",
   description = "Package that will help you organizing a Secret Santa party!",
   keywords = ["secret", "santa", "christmas"],
   author = "Renaud Parent",
   author_email = "renaud.parent@gmail.com",
   url = "https://github.com/RenaudParent/pycret-santa",
-  download_url = "https://github.com/RenaudParent/pycret-santa/tarball/0.2",
+  download_url = "https://github.com/RenaudParent/pycret-santa/tarball/0.3",
   packages = ["pycret_santa"],
   package_data = {"pycret_santa": ["*.yaml"]},
   setup_requires = ["nose>=1.0", "mock", "mox"],

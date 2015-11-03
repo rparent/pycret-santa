c2f4d28278fe8ca3c7779061fa203dd82f9e6c22
Renaud Parent <renaud.parent@visionmark-group.com>
2015-11-03 15:38:43 +0100

    Release 0.5

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 SETTINGS = dict(
   name = "pycret-santa",
-  version = "0.4",
+  version = "0.5",
   description = "Package that will help you organizing a Secret Santa party!",
   keywords = ["secret", "santa", "christmas"],
   author = "Renaud Parent",
   author_email = "renaud.parent@gmail.com",
   url = "https://github.com/RenaudParent/pycret-santa",
-  download_url = "https://github.com/RenaudParent/pycret-santa/tarball/0.3",
+  download_url = "https://github.com/RenaudParent/pycret-santa/tarball/0.5",
   packages = ["pycret_santa"],
   package_data = {"pycret_santa": ["*.yaml"]},
   install_requires = ["pyyaml"],
